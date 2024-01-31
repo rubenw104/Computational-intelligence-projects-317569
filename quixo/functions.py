eea787b3a52dc14f774d9035ffa98389a3ae5fb0
Ruben Tetamo <ruben.w104@gmail.com>
2024-01-31 20:44:50 +0100

    final commit

--- quixo/functions.py
+++ quixo/functions.py
@@ -2,16 +2,15 @@ from game import Game, Move
 from copy import deepcopy, copy
 from random import choice
 import numpy as np
-# import sys
-# sys.setrecursionlimit(sys.getrecursionlimit()*10)
+
 perimeter={(0,0), (1,0), (2,0), (3,0), (4,0),
                (0,1),                      (4,1),
                (0,2),                      (4,2),
                (0,3),                      (4,3),
                (0,4), (1,4), (2,4), (3,4), (4,4)}
+
 def acceptable_move(game:Game,from_pos: tuple[int, int], slide: Move, player_id):
     """given a move(from_pos + slide) and a player, tells if it is acceptable"""
-    #g= deepcopy(game)
     old_r=old_row(game,from_pos,slide)
     acceptable: bool =game._Game__move(from_pos, slide, player_id)
     if acceptable:
@@ -29,50 +28,50 @@ def score(game: Game, player_id, depth):
         #a lose with many steps is less valuable
         return -1000 - depth
     else:
+        #instead of returning 0, I give preference to nodes less deep
         return depth
-def is_symmetric(board1, board2):
-    return np.array_equal(board1, board2) or np.array_equal(np.rot90(board1), board2) or np.array_equal(np.rot90(board1, 2), board2) or np.array_equal(np.rot90(board1, 3), board2) or np.array_equal(np.flip(board1, axis=1), board2) or np.array_equal(np.flip(board1, axis=0), board2)
 
-def available_moves(game: Game, player_id: int, simmetry:bool =False, perimeter=perimeter)->set:
+def is_symmetric(board1, board2) ->bool:
+    return (np.array_equal(board1, board2) or np.array_equal(np.rot90(board1), board2) 
+            or np.array_equal(np.rot90(board1, 2), board2) or np.array_equal(np.rot90(board1, 3), board2) 
+            or np.array_equal(np.flip(board1, axis=1), board2) or np.array_equal(np.flip(board1, axis=0), board2)
+            or np.array_equal(np.rot90(np.flip(board1, axis=1)), board2)
+            or np.array_equal(np.rot90(np.flip(board1, axis=1),3), board2) )
+
+def available_moves(game: Game, player_id: int, simmetry:bool =True, perimeter=perimeter)->set:
     """returns list of possible moves (move, slide)"""
     board= game._board
     availables=set()
     for move in perimeter:
-        #if board[(move[0], move[1])]==player_id or board[(move[0], move[1])]==-1:
-        for slide in Move:
-            if acceptable_move(game, move, slide, player_id):
-                if simmetry:
-                    # move,slide is acceptable-> find the relative board->
-                    # find symmetrical boards -> check if they are already inside availables
-                    
-                    #retrieve board after move
-                    old_r1=old_row(game, move, slide)        
-                    game._Game__move( move, slide, player_id)
-                    board_1=copy(game._board)
-                    undo_move( game,move, slide, old_r1)
-                    
-                    #store the 3 simmetrical values in 3 variables
-                    # board_90=np.rot90(board_1)
-                    # board_180=np.rot90(board_1,2)
-                    # board_270=np.rot90(board_1,3)
-                    # board_oriz=np.flip(board_1, axis=1)
-                    # board_vert=np.flip(board_1, axis=0)
-                    simmetrycal=False
-                    for available in availables:
-                        #retrieve board
-                        old_r2=old_row(game, available[0], available[1])        
-                        game._Game__move( available[0], available[1], player_id)
+        if board[(move[1], move[0])]==player_id or board[(move[1], move[0])]==-1:
+            for slide in Move:
+                if acceptable_move(game, move, slide, player_id):
+                    if simmetry:
+                        # move,slide is acceptable-> find the relative board->
+                        # find symmetrical boards -> check if they are already inside availables
                         
-                        if is_symmetric(board_1, game._board):
-                            simmetrycal=True
+                        #retrieve board after move
+                        old_r1=old_row(game, move, slide)        
+                        game._Game__move( move, slide, player_id)
+                        board_1=copy(game._board)
+                        undo_move( game,move, slide, old_r1)
+                        
+                        simmetrycal=False
+                        for available in availables:
+                            #retrieve board
+                            old_r2=old_row(game, available[0], available[1])        
+                            game._Game__move( available[0], available[1], player_id)
+                            
+                            #checks if there is already a symmetric move
+                            if is_symmetric(board_1, game._board):
+                                simmetrycal=True
 
-                        # if np.array_equal(board_90,game._board) or np.array_equal(board_180,game._board) or np.array_equal(board_270,game._board) or np.array_equal(board_oriz,game._board) or np.array_equal(board_vert,game._board):
-                        #     simmetrycal=True
-                        undo_move( game,available[0], available[1], old_r2)
-                    if simmetrycal == False:
+                            
+                            undo_move( game,available[0], available[1], old_r2)
+                        if simmetrycal == False:
+                            availables.add((move,slide))
+                    else: 
                         availables.add((move,slide))
-                else: 
-                    availables.add((move,slide))
     return availables
 
 def game_over(game: Game)-> bool:
@@ -81,37 +80,6 @@ def game_over(game: Game)-> bool:
         return True
     else:
         return False
-
-def update_player_id(current_player)-> int:
-    if current_player==0:
-        return 1
-    elif current_player==1:
-        return 0
-    else:
-        return None
-
-def minimax(game: Game, player_id):
-    if game_over(game):
-        return score(game, player_id)
-    
-    current_player= player_id
-    scores = [] # an array of scores
-    moves = []  # an array of moves
-
-    if len(available_moves(game, current_player))==0:
-        return 0
-    for (move,slide) in available_moves(game, current_player):
-        g= deepcopy(game)
-        #makes one of the possible moves
-        possible_game = g._Game__move( move, slide, current_player)
-        assert possible_game== True, "Mossa non valida"
-        #changes current player
-        current_player= update_player_id(current_player)
-        #calculates scores by recursion until game_over
-        scores.append(minimax(g, current_player))
-        moves.append((move,slide))
-    
-    return moves,scores
 
 def undo_move(game:Game, move:tuple, slide: Move, old_r):
     """function that undoes the move in input"""
@@ -129,7 +97,8 @@ def old_row(game:Game, move:tuple, slide: Move):
         return deepcopy(game._board[:,move[0]])
 
 
-def minimax2(game:Game, depth:int, player_id:int, is_maximizing_player:bool=True, alfa:float=float('-inf'), beta:float= float('inf'), counter_nodes=0):
+def minimax(game:Game, depth:int, player_id:int, is_maximizing_player:bool=True, alfa:float=float('-inf'), beta:float= float('inf'), counter_nodes=0, simmetry=True):
+    """returns the evaluation of the chosen node, the move associated and the number of explored nodes"""
     counter_nodes+=1
     if depth<=0 or game_over(game)==True:
         return score(game, player_id, depth), None, counter_nodes
@@ -138,12 +107,7 @@ def minimax2(game:Game, depth:int, player_id:int, is_maximizing_player:bool=True
     if is_maximizing_player and game_over(game)==False:
         max_eval = float('-inf')
         best_move = None
-        availables=available_moves(game, player_id, simmetry=True)
-        if len(availables)==0:
-            print("stop")
-            game.print()
-            print()
-            available_moves(game, player_id)
+        availables=available_moves(game, player_id, simmetry)
         for (move,slide) in availables:
             #g=deepcopy(game)
             old_r=old_row(game, move, slide)
@@ -151,7 +115,7 @@ def minimax2(game:Game, depth:int, player_id:int, is_maximizing_player:bool=True
             #g._Game__move( move, slide, player_id)
             game._Game__move( move, slide, player_id)
             #eval,_ = minimax2(g, depth - 1,player_id, False,alfa,beta)
-            eval,_, counter_nodes = minimax2(game, depth - 1,player_id, False,alfa,beta, counter_nodes)
+            eval,_, counter_nodes = minimax(game, depth - 1,player_id, False,alfa,beta, counter_nodes)
             undo_move(game, move,slide, old_r)
 
             if eval > max_eval:
@@ -166,12 +130,7 @@ def minimax2(game:Game, depth:int, player_id:int, is_maximizing_player:bool=True
     elif game_over(game)==False:
         min_eval = float('inf')
         best_move = None
-        availables=available_moves(game, (player_id+1)%2)
-        if len(availables)==0:
-            print("stop")
-            game.print()
-            print()
-            available_moves(game, (player_id+1)%2, simmetry=True)
+        availables=available_moves(game, (player_id+1)%2, simmetry)
         for (move,slide) in availables:
             #g=deepcopy(game)
             old_r=old_row(game, move, slide)
@@ -179,7 +138,7 @@ def minimax2(game:Game, depth:int, player_id:int, is_maximizing_player:bool=True
             #g._Game__move( move, slide, (player_id+1)%2)
             game._Game__move( move, slide, (player_id+1)%2)
             #eval, _ = minimax2(g, depth - 1, player_id, True, alfa, beta)
-            eval, _,counter_nodes = minimax2(game, depth - 1, player_id, True, alfa, beta, counter_nodes)
+            eval, _,counter_nodes = minimax(game, depth - 1, player_id, True, alfa, beta, counter_nodes)
             undo_move(game, move,slide, old_r)
             if eval < min_eval:
                 min_eval = eval
--- quixo/main.py
+++ quixo/main.py
@@ -1,42 +1,31 @@
 import random
 from game import Game, Move, Player
-from copy import deepcopy
-from pprint import pprint
-from functions import acceptable_move,available_moves,score, minimax,update_player_id, game_over,minimax2, old_row, undo_move
-from random import choice
+from functions import  minimax
 from tqdm import tqdm
 import numpy as np
 class RandomPlayer(Player):
     def __init__(self) -> None:
         super().__init__()
-
+        self.name="RandomPlayer"
     def make_move(self, game: 'Game') -> tuple[tuple[int, int], Move]:
         from_pos = (random.randint(0, 4), random.randint(0, 4))
         move = random.choice([Move.TOP, Move.BOTTOM, Move.LEFT, Move.RIGHT])
-        # if game.verbose:
-        #     print("Actual board")
-        #     game.print()
-        #     print(f'Player {game.get_current_player()} does: {from_pos}  MOVE: {move}')
-            
+           
         return from_pos, move
 
 
 class MyPlayer(Player):
-    def __init__(self) -> None:
+    def __init__(self, simmetry=False) -> None:
         super().__init__()
-    
-    
+        self.number_of_nodes=np.array([])
+        self.simmetry=simmetry
+        self.name="MyPlayer"
     def make_move(self, game: 'Game') -> tuple[tuple[int, int], Move]:
-        # from_pos = (random.randint(0, 4), random.randint(0, 4))
-        # move = random.choice([Move.TOP, Move.BOTTOM, Move.LEFT, Move.RIGHT])
-        # try:
-        result=minimax2(game, 2, game.get_current_player(),counter_nodes=0)
+        
+        result=minimax(game, 2, game.get_current_player(),counter_nodes=0, simmetry=self.simmetry)
         eval, (from_pos,move),counter_nodes = result
-            #print(counter_nodes)
-        # except TypeError:
-        #     print("There was an error in minmax, a random move has been taken")
-        #     from_pos = (random.randint(0, 4), random.randint(0, 4))
-        #     move = random.choice([Move.TOP, Move.BOTTOM, Move.LEFT, Move.RIGHT])
+        self.number_of_nodes=np.append(self.number_of_nodes, counter_nodes)
+
         return from_pos, move
 
 def play_games(number_of_games:int , verbose:bool, player1:Player, player2:Player):
@@ -45,52 +34,11 @@ def play_games(number_of_games:int , verbose:bool, player1:Player, player2:Playe
         g = Game(verbose=verbose)
         winner = g.play(player1, player2)
         winners[winner]+=1
-        # g.print()
-        # print(f"Winner: Player {winner}")
         g=None
-    
+    print(f'player1: {player1.name} player2: {player2.name}')
+    print(f'Average of explored nodes with simmetry= {player1.simmetry if isinstance(player1, MyPlayer) else player2.simmetry}: {np.mean(player1.number_of_nodes if isinstance(player1, MyPlayer) else player2.number_of_nodes)}')
     print(f'Player 0 has won {winners[0]} games, Player 1 has won {winners[1]} games. total games: {number_of_games} ')
 
 if __name__ == '__main__':
-    # g = Game(verbose=True)
-    # g.print()
-    # player1 = MyPlayer()
-    # player2 = RandomPlayer()
-    # winner = g.play(player1, player2)
-    # g.print()
-    # print(f"Winner: Player {winner}")
-
-    # print("actual game: ",g._board, sep="\n")
-    # #conserva la vecchia riga
-    # mossa=(0,2), Move.RIGHT
-    # old_r=old_row(g,mossa[0], mossa[1])
-    # print("old row ", old_r, sep=" ")
-    # #fai la mossa
-    # g._Game__move(*mossa,0)
-    # print("game after move: ",g._board, sep="\n")
-    # #annulla la mossa
-    # undo_move(g,mossa[0], mossa[1],old_r)
-    # print("game after undo: ",g._board, sep="\n")
-
-    play_games(100, verbose=False, player1=MyPlayer(), player2=RandomPlayer())
-    play_games(100, verbose=False, player1=RandomPlayer(), player2=MyPlayer())
-    # g=Game(verbose=False)
-    # move=(0,0)
-    # g._Game__move((0,4),Move.TOP,-1)
-    # pprint(g._board)
-    # g= Game(verbose=True)
-    # g.print()
-    # current_player=0
-    # for _ in range(20):
-    #     move,slide=choice(available_moves(g,current_player))
-    #     g._Game__move(move,slide, current_player)
-    #     print(f"Move number:{_}, Player: {current_player}, Move:{move}, Slide: {slide}")
-    #     g.print()
-    #     current_player=update_player_id(current_player)
-    #     if game_over(g):
-    #         break
     
-    # eval, (move,slide) = minimax2(deepcopy(g), 4, True)
-    # print(move,slide,eval, sep=" ")
-    # g=Game(verbose=True)
-    # print(available_moves(g,0))
+    play_games(20, verbose=True, player1=RandomPlayer(), player2=MyPlayer(simmetry=False))
